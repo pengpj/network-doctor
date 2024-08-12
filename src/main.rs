65c2fa6d8ff44d5ba00b4fc33b558d32a551e861
pz <pz>
2024-08-12 19:49:34 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod route_helper;
+mod flannel_helper;
 
 fn main() -> () {
     println!("Hello, world!");
+
+    // 读取 flannel 配置
+    flannel_helper::flannel_config();
+
     // 仅在 linux 系统中执行
     #[cfg(target_os = "linux")]
     route_helper::kubernetes_route();
