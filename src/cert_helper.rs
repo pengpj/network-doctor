cb781fa91eb228fd9e0ed87e287f53696fa93091
pz <pz>
2024-08-19 14:11:48 +0800

    cert

---
+++ src/cert_helper.rs
@@ -0,0 +1,101 @@
+use chrono::{DateTime, NaiveDateTime, ParseError, TimeZone, Utc};
+
+/// for i in `ls /var/lib/rancher/k3s/server/tls/*.crt`; do echo $i; openssl x509 -enddate -noout -in $i; done
+/// 检查证书有效期
+/// 如果证书已经过期，输出证书路径和过期时间
+pub(crate) fn cert_check() {
+    let tls_dir = "/var/lib/rancher/k3s/server/tls";
+    println!("tls dir: {}", tls_dir);
+    let entries = match std::fs::read_dir(tls_dir) {
+        Ok(entries) => entries,
+        Err(e) => {
+            eprintln!("failed to read dir: {}", e);
+            return;
+        }
+    };
+    for entry in entries {
+        let entry = match entry {
+            Ok(entry) => entry,
+            Err(e) => {
+                eprintln!("failed to read entry: {}", e);
+                continue;
+            }
+        };
+        let path = entry.path();
+        let path = match path.to_str() {
+            Some(path) => path,
+            None => {
+                eprintln!("failed to convert path to str");
+                continue;
+            }
+        };
+        // 检查证书有效期
+        let output = match std::process::Command::new("openssl")
+            .arg("x509")
+            .arg("-enddate")
+            .arg("-noout")
+            .arg("-in")
+            .arg(path)
+            .output() {
+            Ok(output) => output,
+            Err(e) => {
+                eprintln!("failed to execute process: {}", e);
+                continue;
+            }
+        };
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            println!("{}: {}", path, stdout);
+            // 解析证书有效期 Jul  1 08:00:00 2022 GMT
+            let parts: Vec<&str> = stdout.split('=').collect();
+            if parts.len() != 2 {
+                eprintln!("invalid output: {}", stdout);
+                continue;
+            }
+            let date = parts[1].trim().to_string();
+            // 解析时间 Jan 10 11:32:20 2026 GMT
+            // let data = "Wed, 18 Feb 2015 23:16:09 GMT";
+            let date = match parse_openssl_time(&date) {
+                Ok(date) => date,
+                Err(e) => {
+                    eprintln!("failed to parse date: {}", e);
+                    continue;
+                }
+            };
+            // 检查是否已经过期
+            let now = chrono::Utc::now();
+            if date < now {
+                eprintln!("{} 证书过期，expired: {}", path, date);
+            }
+            // 打印过期天数
+            let days = date.signed_duration_since(now).num_days();
+            println!("{} 过期天数: {}", path, days);
+        } else {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            eprintln!("failed: {}", stderr);
+            continue;
+        }
+    }
+}
+
+fn parse_openssl_time(time_str: &str) -> Result<DateTime<Utc>, ParseError> {
+    // let time_str = "Jan 10 11:32:20 2020 GMT";
+    // println!("time_str: {}", time_str);
+    let format = "%b %e %H:%M:%S %Y %Z";
+    let naive = NaiveDateTime::parse_from_str(time_str, format);
+    Ok(TimeZone::from_utc_datetime(&Utc, &naive.unwrap()))
+    // Ok(DateTime::<Utc>::from_utc(naive.unwrap(), Utc))
+    // .map(|dt| dt.with_timezone(&Utc));
+    // DateTime::parse_from_str(time_str, format)
+    //     .map(|dt| dt.with_timezone(&Utc))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cert_check() {
+        cert_check();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ mod route_helper;
 mod flannel_helper;
 mod iptables_helper;
 mod curl_helper;
+mod cert_helper;
 
 fn main() -> () {
     println!("Hello, world!");
+
+    // 读取证书有效期
+    cert_helper::cert_check();
 
     // 读取 flannel 配置
     flannel_helper::flannel_config();
@@ -12,6 +16,9 @@ fn main() -> () {
     // 仅在 linux 系统中执行
     #[cfg(target_os = "linux")]
     route_helper::kubernetes_route();
+
+    // 检查 iptables 规则
+    iptables_helper::check_iptables();
 
     // curl ping
     curl_helper::curl_ping();
